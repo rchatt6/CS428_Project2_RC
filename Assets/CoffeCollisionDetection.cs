a6a08556b974e9f387ef12e144e6eb3a51e631ca
R chat <rchatt6@uic.edu>
2022-10-04 00:14:37 -0500

    Added Coffee Implimentation and Changed Load in location

--- Assets/CoffeCollisionDetection.cs
+++ Assets/CoffeCollisionDetection.cs
@@ -6,6 +6,8 @@ public class CoffeCollisionDetection : MonoBehaviour
 {
     //public GameObject item;
     public AudioClip audio;
+    public GameObject item;
+    public GameObject spawnlocation;
     // Start is called before the first frame update
     /*void Start()
     {
@@ -22,5 +24,11 @@ public class CoffeCollisionDetection : MonoBehaviour
     {
         //GameObject e = Instantiate(item) as GameObject;
         AudioSource.PlayClipAtPoint(audio, transform.position);
+        //Instantiate(item, spawnlocation.GetComponent<Transform>().position, spawnlocation.GetComponent<Transform>().rotation);
+
+        Instantiate(item, new Vector3(-0.5f, -.4f, 4.3f), Quaternion.Euler(new Vector3(-90f, 0f, 0f)));
+
+        //GameObject a = Instantiate(item, spawnlocation.GetComponent<Transform>().position, spawnlocation.GetComponent<Transform>().rotation) as GameObject;
+        //a.GetComponent<MeshRenderer>().enabled = true;
     }
 }
