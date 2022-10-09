bc5a1376d1dc9757b6360b0b7dd38490cfe4b604
R chat <rchatt6@uic.edu>
2022-10-09 00:14:19 -0500

    Added Collision Sounds

---
+++ Assets/SoundOnCollision.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnCollision : MonoBehaviour
+{
+    public AudioClip[] audio = new AudioClip[4];
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }*/
+
+    void OnCollisionEnter()
+    {
+        int sound = Random.Range(0, audio.Length);
+        AudioSource.PlayClipAtPoint(audio[sound], transform.position);
+    }
+
+}
