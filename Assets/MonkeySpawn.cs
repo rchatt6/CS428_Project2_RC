396f756b60f27a438f8656e3dc55b4b1ecddb2b2
benhern <43895569+benhern@users.noreply.github.com>
2022-10-13 02:14:41 -0500

    Added in lever and btn animation

--- Assets/MonkeySpawn.cs
+++ Assets/MonkeySpawn.cs
@@ -7,16 +7,18 @@ public class MonkeySpawn : MonoBehaviour
     public AudioClip audio;
     public GameObject item;
     public GameObject spawnlocation;
+    int count = 0;
 
     private void OnTriggerEnter(Collider collider)
     {
-        
+        if(count < 5){
         AudioSource.PlayClipAtPoint(audio, transform.position);
-
         Instantiate(item, spawnlocation.transform.position, Quaternion.identity);
         
         Debug.Log(item.transform.localPosition);
         Debug.Log(item.transform.localRotation);
+        count++;
+        }
 
 
     }
