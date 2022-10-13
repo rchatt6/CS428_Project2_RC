45273a6c7346d49e02ed5b646f50e6e56b58e377
R chat <rchatt6@uic.edu>
2022-10-13 16:53:27 -0500

    Added additional changes

--- Assets/MonkeySpawn.cs
+++ Assets/MonkeySpawn.cs
@@ -13,7 +13,8 @@ public class MonkeySpawn : MonoBehaviour
     {
         if(count < 5){
         AudioSource.PlayClipAtPoint(audio, transform.position);
-        Instantiate(item, spawnlocation.transform.position, Quaternion.identity);
+        //Instantiate(item, spawnlocation.transform.position, Quaternion.identity);
+        Instantiate(item, new Vector3(-3f, 1f, 4f), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
         
         Debug.Log(item.transform.localPosition);
         Debug.Log(item.transform.localRotation);
