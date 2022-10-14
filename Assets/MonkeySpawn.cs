6e87e1e1000b12e830317defa61c9a57af725d90
benhern <43895569+benhern@users.noreply.github.com>
2022-10-14 15:28:24 -0500

    Added in levers and portals

--- Assets/MonkeySpawn.cs
+++ Assets/MonkeySpawn.cs
@@ -6,19 +6,26 @@ public class MonkeySpawn : MonoBehaviour
 {
     public AudioClip audio;
     public GameObject item;
-    public GameObject spawnlocation;
+    public GameObject portal;
+
     int count = 0;
 
     private void OnTriggerEnter(Collider collider)
     {
         if(count < 5){
-        AudioSource.PlayClipAtPoint(audio, transform.position);
-        //Instantiate(item, spawnlocation.transform.position, Quaternion.identity);
-        Instantiate(item, new Vector3(-3f, 1f, 4f), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
-        
-        Debug.Log(item.transform.localPosition);
-        Debug.Log(item.transform.localRotation);
-        count++;
+            portal.SetActive(true);
+            AudioSource.PlayClipAtPoint(audio, transform.position);
+            //Instantiate(item, spawnlocation.transform.position, Quaternion.identity);
+            Instantiate(item, new Vector3(-3f, 1f, 4f), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
+            
+            Debug.Log(item.transform.localPosition);
+            Debug.Log(item.transform.localRotation);
+            count++;
+        }
+
+        else{
+            portal.GetComponent<Animator>().Play("Base Layer.Portal Disappearing");
+
         }
 
 
